a979c61fadcc58a6882873f572ca7427b748e5dd
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-02-15 13:17:41 +0600

    added random alphabet functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 function playNow () {
     getId('play-game').classList.remove('hidden');
     getId('play-now').classList.add('hidden')
-    console.log();
+    continueGame()
+}
+
+function continueGame() {
+    getId('alpha-screen').innerText = randomAlphabet();
 }
 
 
--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,10 @@
 function getId (idName){
     return document.getElementById(idName);
 }
+
+function randomAlphabet () {
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabet = alphabets.split('')
+    const random = Math.floor(Math.random() * alphabet.length);
+    return alphabet[random];
+}
